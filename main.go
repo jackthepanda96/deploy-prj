39daa8cfd6a5ff44087959d98ed375246cb84fa0
Jerry Young <jerryyoung2911@gmail.com>
2022-10-13 09:13:30 +0700

    Add logger

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
-package docker
+package main
 
 import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
 	e.GET("/hello", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "hello world")
 	})
